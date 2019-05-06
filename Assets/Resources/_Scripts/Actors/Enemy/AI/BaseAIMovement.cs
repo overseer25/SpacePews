8ac60a38753f93fc1fb74e8ca8c3f6f496366543
Robert Chandler Waugh <chandler@gbpackerfans.com>
2019-05-05 19:13:37 -0600

    Starting base AI movement script. Need to decide if movement is always affected by time.detla time or if it will be a full unit each time and the passed in value can be a value calculated by time.delta time.

---
+++ Assets/Resources/_Scripts/Actors/Enemy/AI/BaseAIMovement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BaseAIMovement : MonoBehaviour
+{
+    private float speed = 0;
+
+    private Vector2 direction = Vector2.up;
+
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Function to always increase the speed of the ship.
+    /// </summary>
+    /// <param name="amount">Amount to increase the speed by. Amount is run through absolute value, and so will always be postive.</param>
+    /// <returns></returns>
+    public float IncreaseSpeed(float amount = 1.0f)
+    {
+        speed += Math.Abs(amount);
+        return speed;
+    }
+
+    /// <summary>
+    /// Function to always decrease the speed of the ship.
+    /// </summary>
+    /// <param name="amount">Amount to decrease the speed by. Amount is run through absolute value, and so will always be postive.</param>
+    /// <returns></returns>
+    public float DecreaseSpeed(float amount = 1.0f)
+    {
+        speed -= Math.Abs(amount);
+        return speed;
+    }
+
+    public void MoveForwardOne()
+    {
+        rigidbody.transform.Translate(direction * Time.deltaTime);
+    }
+
+    public void MoveForward(float amount = 1.0f)
+    {
+
+    }
+}
